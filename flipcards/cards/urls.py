c3dc91681f9202cf5e4fb1fd912593906afed155
Paulo Marques <pepcmarques@gmail.com>
2021-10-15 16:31:29 -0700

    topics new (initial)

--- flipcards/cards/urls.py
+++ flipcards/cards/urls.py
@@ -24,4 +24,8 @@ urlpatterns = [
     path('collection/new/', views.add_collection, name="add_collection"),
     path('collection/<int:some_id>/change/', views.edit_collection, name="edit_collection"),
     path('collection/<int:some_id>/delete/', views.delete_collection, name="delete_collection"),
+    #
+    path('collection/<int:some_id>/topic/new/', views.handle_dashboard, name="add_topic"),
+    #path('collection/<int:some_id>/topic/<int:another_id>/change/', views.handle_dashboard, name="edit_topic"),
+    #path('collection/<int:some_id>/topic/<int:another_id>/delete/', views.handle_dashboard, name="delete_topic"),
 ]
